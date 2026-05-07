05f6129520ae6f7fd9fddc8367ac568c32e82839
Jerzy Trcka <jerzy.trcka@gmail.com>
2026-05-07 18:16:25 +0200

    Add board

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,8 @@
 import androidx.compose.runtime.*
+import org.jetbrains.compose.web.attributes.*
 import org.jetbrains.compose.web.dom.Button
 import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Input
 import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
 
@@ -12,17 +14,71 @@ fun main() {
 
 @Composable
 fun Body() {
-    var counter by remember { mutableStateOf(0) }
+    var firstNumber by remember { mutableStateOf<Number?>(null) }
+    var secondNumber by remember { mutableStateOf<Number?>(null) }
+    var gridSize by remember { mutableStateOf(0) }
+    var blueToMove : Boolean by remember { mutableStateOf(false) }
+    var hoveredColumn by remember { mutableStateOf<Int?>(null) }
+
     Div {
-        Text("Clicked: ${counter}")
+        Text("Size of board:")
     }
-    Button(
+    Input(
+        type = InputType.Number,
         attrs = {
-            onClick { _ ->
-                counter++
+            onInput { event ->
+                firstNumber = event.value
             }
         }
-    ) {
-        Text("Click")
+    )
+
+    Div {
+        Text("Win condition (how many in a row):")
+    }
+    Input(
+        type = InputType.Number,
+        attrs = {
+            onInput { event ->
+                secondNumber = event.value
+            }
+        }
+    )
+
+    Div {
+        Button(
+            attrs = {
+                onClick { _ ->
+                    gridSize = firstNumber?.toInt() ?: 0
+                }
+            }
+        ) {
+            Text("Start")
+        }
+    }
+
+    if (gridSize > 0) {
+        Div(attrs = {
+            classes("board")
+            onMouseLeave { hoveredColumn = null }
+        }) {
+            repeat(gridSize) {
+                Div(attrs = { classes("square-row") }) {
+                    repeat(gridSize) { columnIndex ->
+                        Div(attrs = {
+                            classes("square")
+                            if (hoveredColumn == columnIndex) {
+                                classes("highlightedSquare")
+                            }
+                            onMouseEnter { hoveredColumn = columnIndex }
+                            onClick { blueToMove = !blueToMove }
+                        }) {
+                        }
+                    }
+                }
+            }
+        }
+    }
+    Div {
+        Text(if (blueToMove) "BLUE to move" else "RED to move")
     }
 }
